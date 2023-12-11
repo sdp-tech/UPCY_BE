c804ee22c37746ce4506a8d422409a6bc33eb85a
SeoJimin <jnuni004@naver.com>
2023-12-11 23:07:18 +0900

    feat: Sign Up for Reformer eccept certification

---
+++ users/services.py
@@ -0,0 +1,72 @@
+import os
+import string
+import random
+import datetime
+
+import io
+import time
+import uuid
+
+from django.conf import settings
+# from django.core.mail import EmailMultiAlternatives
+# from django.utils.encoding import force_str, force_bytes
+# from django.utils.http import urlsafe_base64_encode
+# from django.template.loader import render_to_string
+
+from rest_framework import exceptions
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_jwt.settings import api_settings
+from django.core.files.images import ImageFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from users.models import User
+# from core.exceptions import ApplicationError
+
+
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+class UserService:
+    def __init__(self):
+        pass
+
+    def reformer_sign_up(
+            email: str, 
+            password: str, 
+            nickname:str, 
+            phone:str, 
+            agreement_terms:bool,
+            school:str,
+            is_enrolled:str,
+            area:str,
+            career:str,
+            work_style:list[str],
+            bios:str,
+            certificate_studentship: InMemoryUploadedFile,
+            ):
+
+        ext = certificate_studentship.name.split(".")[-1]
+        file_path = '{}.{}'.format(str(time.time())+str(uuid.uuid4().hex), ext)
+        certificate_studentship = ImageFile(io.BytesIO(certificate_studentship.read()), name=file_path)
+
+        user = User(
+            email = email,
+            nickname = nickname,
+            password = password,
+            phone = phone,
+            is_reformer=True,
+            agreement_terms=agreement_terms,
+            school = school,
+            is_enrolled = is_enrolled,
+            area = area,
+            career = career,
+            bios = bios,
+            certificate_studentship=certificate_studentship,
+        )
+
+        user.set_password(password)
+        user.is_active = False
+        user.save()
+
+        user.work_style.set(work_style)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework import serializers
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+
+from users.models import User
+from users.services import UserService
+
 # Create your views here.
+
+class ReformerSignUpApi(APIView):
+    permission_classes = (AllowAny,)
+
+    class ReformerSignupInputSerializer(serializers.Serializer):
+        #TODO재학증명서, 전화번호 인증 구현 필요
+        email = serializers.EmailField()
+        password = serializers.CharField()
+        nickname = serializers.CharField()
+        phone = serializers.CharField()
+        agreement_terms = serializers.BooleanField()
+        school = serializers.CharField()
+        is_enrolled = serializers.CharField()
+        area = serializers.CharField()
+        career = serializers.CharField()
+        work_style = serializers.ListField()
+        bios = serializers.CharField()
+        certificate_studentship = serializers.ImageField()
+
+    def post(self, request):
+        serializer = self.ReformerSignupInputSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.validated_data
+
+        UserService.reformer_sign_up(
+            email = data.get('email'),
+            password = data.get('password'),
+            nickname = data.get('nickname'),
+            phone = data.get('phone'),
+            agreement_terms= data.get('agreement_terms'),
+            school = data.get('school'),
+            is_enrolled = data.get('is_enrolled'),
+            area = data.get('area'),
+            career = data.get('career'),
+            work_style=data.get('work_style'),
+            bios = data.get('bios'),
+            certificate_studentship = data.get('certificate_studentship'),
+        )
+
+        return Response({
+            'stauts': 'success',
+        },status = status.HTTP_201_CREATED)

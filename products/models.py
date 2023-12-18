f6990d137acb07021ca3499d98cdcf67b524c420
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2023-12-19 00:50:57 +0900

    feat: MakeProductModel (#3)
    Co-authored-by: SeoJimin <jnuni004@naver.com>

---
+++ products/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from core.models import TimeStampedModel
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank= False)
+
+class Product(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', related_name='products', on_delete=models.CASCADE, null=False, blank=False)
+    basic_price = models.CharField(max_length=500, blank = False)
+    option = models.TextField(blank = True)
+    info = models.TextField(blank = True)
+    notice = models.TextField(blank = True)
+    period = models.CharField(max_length=100, blank = True)
+    transaction_direct = models.BooleanField(default=False)
+    transaction_package = models.BooleanField(default=False)
+    refund = models.TextField(blank = True)
+    reformer = models.ForeignKey('users.User', related_name='products', on_delete=models.SET_NULL, null=True, blank=False)
+
+
+
+class ProductKeyword(models.Model):
+    name = models.CharField(max_length=100)
+    product = models.ForeignKey(
+        'Product', related_name='keywords', on_delete=models.CASCADE, null=False, blank=False)
+    
+
+#Product_Photo 모델 만들기
+def get_product_photo_upload_path(instance, filename):
+    return 'product/photo/{}'.format(filename)
+
+class ProductPhoto(models.Model):
+    image = models.ImageField(
+        upload_to=get_product_photo_upload_path, default='product_photo.png')
+    product = models.ForeignKey(
+        'Product', related_name='product_photos', on_delete=models.CASCADE, null=False, blank=False)

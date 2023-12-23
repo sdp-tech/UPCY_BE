8deae40f3db03315a92f73f30d30dcbc6e229879
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2023-12-23 15:48:28 +0900

    Make Consumer Signup APIonsumer (#6)
    * feat: Make Consumer Signup API
    
    * style: Remove Commentary
    
    ---------
    
    Co-authored-by: SeoJimin <jnuni004@naver.com>

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
-from .views import ReformerSignUpApi
+from .views import ReformerSignUpApi, ConsumerSignUpApi
 app_name = "users"
 
 urlpatterns =[
     path("reformer_signup/", ReformerSignUpApi.as_view(), name = "reformer_signup"),
+    path("consumer_signup/", ConsumerSignUpApi.as_view(), name = "consumer_signup"),
 ]

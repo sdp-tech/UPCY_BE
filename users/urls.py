bed1d76139dc2caa49c51bd3399f575f36a307c1
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2023-12-23 16:44:10 +0900

    feat: UserLogin (#7)
    Co-authored-by: SeoJimin <jnuni004@naver.com>

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
-from .views import ReformerSignUpApi, ConsumerSignUpApi
+from .views import ReformerSignUpApi, ConsumerSignUpApi, UserLoginApi
 app_name = "users"
 
 urlpatterns =[
     path("reformer_signup/", ReformerSignUpApi.as_view(), name = "reformer_signup"),
     path("consumer_signup/", ConsumerSignUpApi.as_view(), name = "consumer_signup"),
+    path('login/', UserLoginApi.as_view(), name = 'login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

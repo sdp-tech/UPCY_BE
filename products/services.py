aac0c4a44fcd5016e58d768a59a5f53bad986ff5
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2024-01-20 16:19:59 +0900

    feat: Modify Reformer SIgnUp to Match IA (#16)
    Co-authored-by: SeoJimin <jnuni004@naver.com>

--- products/services.py
+++ products/services.py
@@ -11,7 +11,7 @@ from django.conf import settings
 
 from products.models import Product, ProductKeyword, ProductPhoto
 from users.models import User
-from .selectors import ProductSelector
+# from .selectors import ProductSelector
 #from core.exceptions import ApplicationError
 
 class ProductCoordinatorService:

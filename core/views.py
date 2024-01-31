8a5bc960b8b1465bc055e88c372bbb46f3e34737
0321minji <0321minji@ewhain.net>
2024-01-31 21:53:43 +0900

    #8 feat : create products/list api & modify some models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+
+
+def get_paginated_response(*, pagination_class, serializer_class, queryset, request, view):
+    paginator = pagination_class()
+
+    page = paginator.paginate_queryset(queryset, request, view=view)
+
+    if page is not None:
+        serializer = serializer_class(page, many=True, context={'request': request})
+    else:
+        serializer = serializer_class(queryset, many=True, context={'request': request})
+
+    return Response({
+        'status': 'success',
+        'data': paginator.get_paginated_response(serializer.data).data,
+    }, status=status.HTTP_200_OK)

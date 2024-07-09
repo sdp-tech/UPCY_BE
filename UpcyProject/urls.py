7aa634e7c1a9709ef3565b1caa0e5f0f90bb656e
chaechaeis <hch011007@ewhain.net>
2024-07-09 19:34:58 +0900

    #61 feat : link Docker

--- UpcyProject/urls.py
+++ UpcyProject/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework.permissions import AllowAny
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
     permission_classes=(AllowAny,),
 )
 urlpatterns = [
-    path(r'swagger(?P<format>\.json|\.yaml$)/',schema_view.without_ui(cache_timeout=0),name='schema-json'),
+    re_path(r'swagger(?P<format>\.json|\.yaml$)/',schema_view.without_ui(cache_timeout=0),name='schema-json'),
     path(r'swagger/',schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui'),
     path(r'redoc/',schema_view.with_ui('redoc',cache_timeout=0),name='schema-redoc'),
     path('admin/', admin.site.urls),

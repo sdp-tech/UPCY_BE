0327ffb0c2ccf0a0537b0952bf15d3ae66f1fe8a
0321minji <0321minji@ewhain.net>
2024-07-13 14:40:14 +0900

    #59 fix : separate settings.py

---
+++ UpcyProject/settings/production.py
@@ -0,0 +1,19 @@
+from settings import *
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}

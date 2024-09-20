b219cb5c70fc2952d4ae09062e3cbc0f0f8c501b
Scanf-s <calzone0404@gmail.com>
2024-09-20 22:51:22 +0900

    User api 수정, Reformer API 수정, users 소스코드 리팩토링

--- market/urls.py
+++ market/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
+from market.views.market_create_list_view import MarketCreateListView
 from market.views.market_crud_view import MarketCrudView
 from market.views.market_service_view import MarketServiceView
 
 urlpatterns = [
-    path("", MarketCrudView.as_view(), name="market_crud"),
-    path("/<uuid:market_uuid>", MarketServiceView.as_view(), name="service_view")
+    path("", MarketCreateListView.as_view(), name="market_create_list"),
+    path("/<uuid:market_uuid>", MarketCrudView.as_view(), name="market_crud")
 ]

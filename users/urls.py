0d8651ea548184b0c5c17a721b9e91b24106dc6d
Scanf-s <calzone0404@gmail.com>
2024-09-28 23:37:28 +0900

    Reformer education API 개발

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from users.views.reformer_view import ReformerProfileView
-from users.views.token_view import *
-from users.views.user_auth_view import *
-from users.views.user_crud_view import *
+from users.views.reformer_view.reformer_education_view.reformer_education_document_view import \
+    ReformerEducationDocumentView
+from users.views.reformer_view.reformer_profile_view import ReformerProfileView
+from users.views.reformer_view.reformer_education_view.reformer_education_create_list_view import ReformerEducationCreateListView
+from users.views.reformer_view.reformer_education_view.reformer_education_view import ReformerEducationView
+from users.views.token_view.token_view import UserTokenVerifyView, UserTokenRefreshView
+from users.views.user_view.user_auth_view import *
+from users.views.user_view.user_crud_view import *
 
 app_name = "users"
 
@@ -15,5 +19,8 @@ urlpatterns =[
     path('/token/verify', UserTokenVerifyView.as_view(), name='token_verify'),
     path('/token/refresh', UserTokenRefreshView.as_view(), name='token_refresh'),
     path('/reformer', ReformerProfileView.as_view(), name='reformer'),
+    path('/reformer/education', ReformerEducationCreateListView.as_view(), name='reformer_education'),
+    path('/reformer/education/<uuid:education_uuid>', ReformerEducationView.as_view(), name='reformer_education_detail'),
+    path('/reformer/education/<uuid:education_uuid>/document', ReformerEducationDocumentView.as_view(), name='reformer_education_document'),
     path('/profile-image', UserImageUploadView.as_view(), name='upload_profile_image'),
 ]
---
+++ users/views/reformer_view/reformer_education_view/reformer_education_create_list_view.py
@@ -0,0 +1,62 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.db import IntegrityError
+from rest_framework.response import Response
+from rest_framework import status
+
+from users.models.reformer import Reformer, ReformerEducation
+from users.serializers.reformer_serializer.reformer_profile_serializer import ReformerEducationSerializer
+
+
+class ReformerEducationCreateListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            reformer_education = ReformerEducation.objects.filter(reformer=request.user.reformer_profile)
+            if not reformer_education:
+                raise Reformer.DoesNotExist
+
+            serializer = ReformerEducationSerializer(instance=reformer_education, many=True)
+            return Response(
+                data=serializer.data,
+                status=status.HTTP_200_OK
+            )
+        except AttributeError as e:
+            return Response(
+                data={'message': str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except Reformer.DoesNotExist:
+            return Response(
+                data={"message": "해당 User가 생성한 Reformer 프로필 정보가 존재하지 않습니다."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+    def post(self, request):
+        try:
+            reformer = Reformer.objects.filter(user=request.user).first()
+            if not reformer:
+                raise Reformer.DoesNotExist
+
+            serializer = ReformerEducationSerializer(data=request.data, context={"reformer":reformer})
+            if serializer.is_valid():
+                serializer.save()
+                return Response(data={'message':'successfully created'}, status=status.HTTP_201_CREATED)
+            else:
+                raise ValueError(f"{serializer.errors}")
+        except (AttributeError, ValueError) as e:
+            return Response(
+                data={'message': str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except IntegrityError as e:
+            return Response(
+                data={'message': '데이터베이스 무결성 오류'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except Reformer.DoesNotExist:
+            return Response(
+                data={"message": "해당 User가 생성한 Reformer 프로필 정보가 존재하지 않습니다."},
+                status=status.HTTP_404_NOT_FOUND
+            )

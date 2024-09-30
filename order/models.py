c0d778fe2d1470a2b176f28e706493b7293f7e9e
SULLUNG <sullung2yo@gmail.com>
2024-09-30 22:14:52 +0900

    110 feat : modify order models.py

--- order/models.py
+++ order/models.py
@@ -9,38 +9,19 @@ class Order(TimeStampedModel):
         on_delete=models.SET_NULL,
         related_name="service_order",
         null=True,
-    )  # 어떤 서비스에 대한 주문인지?
+    )  # 어떤 서비스에 대한 주문인지
     request_user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="request_user_order"
     )  # 주문한 사람
-    request_detail = models.TextField(null=False)  # 기본 요구 사항
-    delivery_address = models.TextField(null=False)  # 결과물 배송지
-    size = models.CharField(max_length=50, null=False)  # 리폼 의류 사이즈
     additional_option = models.ForeignKey(
         "market.ServiceOption",
         on_delete=models.SET_NULL,
         related_name="additional_option_order",
         null=True,
     )
+    additional_request = models.TextField(null=True)  # 추가 요청 사항
     total_price = models.IntegerField(null=False)  # 최종 리폼 견적 금액
     request_date = models.DateField(null=False)  # 주문 시간
-    due_date = models.DateField(null=False)  # 마감 시간
-    transaction_option = models.CharField(
-        max_length=50, null=False, choices=[("pickup", "대면"), ("delivery", "택배")]
-    )  # 거래 방식 (택배 or 대면)
-    request_canceled = models.BooleanField(default=False)  # 주문 취소 여부
-    portfolio_status = models.BooleanField(
-        default=False
-    )  # 해당 주문 포트폴리오 사용 가능 여부
-    additional_request = models.TextField(null=True)  # 추가 요청 사항
-    payment_status = models.BooleanField(default=False)  # 입금 여부
-    material_received_status = models.BooleanField(
-        default=False
-    )  # 주문자가 발송한 재료를 리포머가 수령했는지에 대한 여부
-    delivery_company = models.CharField(max_length=50, null=True)  # 택배 회사
-    delivery_tracking_number = models.CharField(
-        max_length=50, null=True
-    )  # 택배 송장 번호
     kakaotalk_openchat_link = models.TextField(null=True)  # 카톡 오픈채팅 링크
 
     class Meta:
@@ -48,7 +29,7 @@ class Order(TimeStampedModel):
 
 
 class OrderImage(TimeStampedModel):
-    # 리폼 요구사항 작성 시 첨부하는 이미지를 관리하는 테이블
+    # 리폼할 의류 이미지를 관리하는 테이블
     order = models.ForeignKey(
         "order.Order", on_delete=models.CASCADE, related_name="order_image"
     )
@@ -57,31 +38,78 @@ class OrderImage(TimeStampedModel):
     class Meta:
         db_table = "order_image"
 
-
-class OrderCategory(TimeStampedModel):
-    # 리폼 요구사항 카테고리 정보를 저장하는 테이블
+class AdditionalImage(TimeStampedModel):
+    #추가 요청사항 이미지를 관리하는 테이블
     order = models.ForeignKey(
-        "order.Order", on_delete=models.CASCADE, related_name="order_category"
+        "order.Order", on_delete=models.CASCADE, related_name="additional_image"
     )
-    category = models.CharField(max_length=50, null=False)  # 주문 카테고리
+    image = models.FileField(upload_to="additional_image") # 수정 필요
+
+    class Meta:
+        db_table = "additional_image"
+
+
+class OrderState(TimeStampedModel):
+    #주문 상태를 관리하는 테이블
+    order = models.ForeignKey(
+        "order.Order", on_delete=models.CASCADE, related_name="order_state"
+    )
+    reformer_status = models.CharField(
+        max_length=10,
+        choices=[("accepted", "수락"), ("rejected", "거절"), ("pending", "대기")
+            , ("received", "재료 수령"), ("produced", "제작완료"),("deliver","배송중")
+                 ,("end","거래 완료")],
+        default="pending",
+    )
+
+    class Meta:
+        db_table = "order_state"
+
+
+class TransactionOption(TimeStampedModel):
+    #거래방식 정보를 관리하는 테이블
+    order = models.ForeignKey(
+        "order.Order", on_delete=models.CASCADE, related_name="transaction_option"
+    )
+    transaction_option = models.CharField(
+        max_length=50, null=False, choices=[("pickup", "대면"), ("delivery", "택배")]
+    )  # 거래 방식 (택배 or 대면)
+    delivery_address = models.TextField(null=False)  # 결과물 배송지
+    delivery_name = models.TextField(null=False) #배송 받을 이름
+    delivery_phone_number= models.TextField(null=False) #배송 받을 전화번호
+
+    class Meta:
+        db_table = "transaction_option"
+
+
+class DeliveryInformation(TimeStampedModel):
+    #택배 정보를 관리하는 테이블
+    order = models.ForeignKey(
+        "order.Order", on_delete=models.CASCADE, related_name="delivery_information"
+    )
+    delivery_company = models.CharField(max_length=50, null=True)  # 택배 회사
+    delivery_tracking_number = models.CharField(
+        max_length=50, null=True
+    )  # 택배 송장 번호
+
+    class Meta:
+        db_table = "delivery_information"
+
+
+
+class OptionCategory(TimeStampedModel):
+    # 옵션 카테고리 정보를 저장하는 테이블
+    order = models.ForeignKey(
+        "order.Order", on_delete=models.CASCADE, related_name="option_category"
+    )
+    option_category = models.CharField(max_length=50, null=False)  # 옵션 카테고리
 
     class Meta:
         db_table = "order_category"
 
 
-class OrderReformStyle(TimeStampedModel):
-    # 리폼 스타일 정보를 저장하는 테이블
-    order = models.ForeignKey(
-        "order.Order", on_delete=models.CASCADE, related_name="order_style"
-    )
-    style_name = models.CharField(max_length=50, null=False)  # 스타일 이름
-
-    class Meta:
-        db_table = "order_reform_style"
-
-
 class OrderReformTexture(TimeStampedModel):
-    # 리폼할 의류의 재료 정보? 재질 정보? 를 저장하는 테이블
+    # 리폼할 의류의 재질정보를 저장하는 테이블
     order = models.ForeignKey(
         "order.Order", on_delete=models.CASCADE, related_name="order_texture"
     )
@@ -90,16 +118,6 @@ class OrderReformTexture(TimeStampedModel):
     class Meta:
         db_table = "order_reform_texture"
 
-
-class OrderReformFit(TimeStampedModel):
-    # 리폼할 의류의 핏 정보를 저장하는 테이블
-    order = models.ForeignKey(
-        "order.Order", on_delete=models.CASCADE, related_name="order_fit"
-    )
-    fit_name = models.CharField(max_length=50, null=False)  # 핏 이름
-
-    class Meta:
-        db_table = "order_reform_fit"
 
 
 # 리뷰 관련된 내용은 2차 개발 기간에..

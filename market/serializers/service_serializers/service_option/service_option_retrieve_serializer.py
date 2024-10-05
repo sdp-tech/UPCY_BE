f1a9e315e22aabb9db3e5be96e3922469356c0fa
Scanf-s <calzone0404@gmail.com>
2024-10-05 13:20:35 +0900

    1. Migration 에러 해결 2. service option api 수정

--- market/serializers/service_serializers/service_option/service_option_retrieve_serializer.py
+++ market/serializers/service_serializers/service_option/service_option_retrieve_serializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from market.models import ServiceOption
+from market.models import ServiceOption, ServiceOptionImage
 
+
+class ServiceOptionImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceOptionImage
+        fields = ["image"]
 
 class ServiceOptionRetrieveSerializer(serializers.ModelSerializer):
+    service_option_image = ServiceOptionImageSerializer(many=True)
+
     class Meta:
         model = ServiceOption
-        fields = ["option_uuid", "option_name", "option_content", "option_price"]
+        fields = ["option_uuid", "option_name", "option_content", "option_price", "service_option_image"]

1032d2c29bd7880ce5f2c8d4eb4857cfdea5cd28
Sullung <calzone0404@gmail.com>
2024-11-09 14:12:18 +0900

    Code lint 적용

--- market/serializers/service_serializers/service_option/service_option_retrieve_serializer.py
+++ market/serializers/service_serializers/service_option/service_option_retrieve_serializer.py
@@ -8,9 +8,16 @@ class ServiceOptionImageSerializer(serializers.ModelSerializer):
         model = ServiceOptionImage
         fields = ["image"]
 
+
 class ServiceOptionRetrieveSerializer(serializers.ModelSerializer):
     service_option_image = ServiceOptionImageSerializer(many=True)
 
     class Meta:
         model = ServiceOption
-        fields = ["option_uuid", "option_name", "option_content", "option_price", "service_option_image"]
+        fields = [
+            "option_uuid",
+            "option_name",
+            "option_content",
+            "option_price",
+            "service_option_image",
+        ]
--- order/services.py
+++ order/services.py
@@ -6,7 +6,8 @@ from django.core.exceptions import ValidationError
 from django.db import transaction
 
 from market.models import Service
-from order.models import (Order, OrderImage, AdditionalImage)
+from order.models import AdditionalImage, Order, OrderImage
+
 
 def validate_image_files(image_files: List) -> None:
     """이미지 파일의 유효성을 검사하는 함수"""
@@ -15,8 +16,7 @@ def validate_image_files(image_files: List) -> None:
             raise ValidationError("Image file size must be less than 10MB")
 
 
-
-class OrderImageUploadService :
+class OrderImageUploadService:
     def __init__(self):
         pass
 
@@ -30,7 +30,7 @@ class OrderImageUploadService :
         try:
             images = []
 
-            #파일 유효성 검증
+            # 파일 유효성 검증
             validate_image_files(image_files)
 
             # 엔티티 타입 별 쿼리 생성
@@ -42,10 +42,11 @@ class OrderImageUploadService :
 
             elif isinstance(entity, AdditionalImage):
                 for image_file in image_files:
-                    images.append(AdditionalImage(service_order=entity, image=image_file))
+                    images.append(
+                        AdditionalImage(service_order=entity, image=image_file)
+                    )
                 # AdditionalImage > Bulk create
                 AdditionalImage.objects.bulk_create(images)
-
 
         except ValidationError as e:
             raise ValidationError(f"Validation Error: {str(e)}")
@@ -60,23 +61,20 @@ class OrderImageUploadService :
         & DB에 저장하는 함수
         """
         try:
-            if image_file.size > 10 * 1024 * 1024: # 10MB 미만의 이미지
+            if image_file.size > 10 * 1024 * 1024:  # 10MB 미만의 이미지
                 raise ValidationError("Image file size must be less than 10MB")
 
             # 엔티티 타입별 이미지 객체 생성
 
             order_image = OrderImage.objects.create(
-                service_order=service_order, image = image_file
+                service_order=service_order, image=image_file
             )
             order_image.save()
             print(
                 "Successfully uploaded service option image"
-            ) # 추후 로그 출력으로 수정
+            )  # 추후 로그 출력으로 수정
 
         except ValidationError as e:
             raise ValidationError(f"Validation Error: {str(e)}")
         except Exception as e:
             raise e
-
-
-

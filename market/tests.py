32a4f86a1e7c11e1499480499d81fc33bf1f7608
Sullung <calzone0404@gmail.com>
2024-11-23 16:58:15 +0900

    Market test code 추가

---
+++ market/tests.py
@@ -0,0 +1,136 @@
+from rest_framework.test import APIClient, APITestCase
+
+from market.models import Market
+from users.models.user import User
+from users.models.reformer import Reformer
+
+
+class MarketTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.test_user = User.objects.create_user(
+            email="test@test.com",
+            password="123123",
+            phone="01012341234",
+            nickname="nickname",
+            introduce="hello, django",
+            role="reformer",
+            is_active=True,
+            agreement_terms=True,
+        )
+        self.reformer = Reformer.objects.create(
+            user=self.test_user,
+            reformer_link="www.naver.com",
+            reformer_area="Seoul"
+        )
+        self.token = self.client.post(
+            path=f"/api/user/login",
+            data={
+                "email": "test@test.com",
+                "password": "123123"
+            }
+        )
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.token.data["access"])
+
+    def test_market_create_simple(self):
+        # 단순 마켓 생성 테스트
+        response = self.client.post(
+            path="/api/market",
+            data={
+                "market_name": "test market",
+                "market_introduce": "Seoul",
+                "market_address": "Incheon",
+            },
+            format="json",
+        )
+
+        market_uuid: str = response.data["market_uuid"]
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Market.objects.filter(market_uuid=market_uuid).count(), 1)
+
+    def test_market_create_without_reformer(self):
+        # 리포머 정보가 없는 사용자가 마켓 생성 시도할 때 막혀야함
+        # 1. 일반 사용자 생성
+        customer_client = APIClient()
+        customer = User.objects.create_user(
+            email="customer@test.com",
+            password="123123",
+            phone="01012341234",
+            nickname="nickname",
+            introduce="hello, django",
+            role="customer",
+            is_active=True,
+            agreement_terms=True,
+        )
+        # 2. 로그인 및 인증 수행
+        response = self.client.post(
+            path="/api/user/login",
+            data={
+                "email": customer.email,
+                "password": "123123"
+            },
+            format="json"
+        )
+        customer_client.credentials(HTTP_AUTHORIZATION="Bearer " + response.data["access"])
+        self.assertEqual(User.objects.filter(email=customer.email).first().role, "customer")
+
+        response = customer_client.post(
+            path="/api/market",
+            data={
+                "market_name": "Invalid market",
+                "market_introduce": "SHOULD NOT CREATED",
+                "market_address": "ABC",
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 403) # IsReformer 권한 검증 시 막혀야 한다.
+
+    def test_forbid_market_create_more_than_one(self):
+        # 이미 마켓이 존재하는데 마켓을 또 생성하는 경우 금지해야함
+        response = self.client.post(
+            path="/api/market",
+            data={
+                "market_name": "test market",
+                "market_introduce": "Seoul",
+                "market_address": "Incheon",
+            },
+            format="json",
+        ) # 마켓 한개 생성 -> 성공
+        self.assertEqual(response.data["market_name"], "test market")
+        self.assertEqual(Market.objects.filter(market_name="test market").first().market_name, "test market")
+
+        # 또 생성하는 경우 에러 발생해야함
+        invalid_response = self.client.post(
+            path="/api/market",
+            data={
+                "market_name": "invalid market",
+                "market_introduce": "13123123",
+                "market_address": "invalid"
+            },
+            format="json"
+        )
+        self.assertEqual(invalid_response.status_code, 400)
+
+    def test_get_market_info(self):
+        # 마켓 정보 생성
+        self.client.post(
+            path="/api/market",
+            data={
+                "market_name": "test market",
+                "market_introduce": "Seoul",
+                "market_address": "Incheon",
+            },
+            format="json",
+        )
+
+        response = self.client.get(
+            path="/api/market",
+            format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['market_name'], "test market")
+        self.assertEqual(response.data['market_introduce'], "Seoul")
+        self.assertEqual(response.data['market_thumbnail'], None)
+        self.assertEqual(response.data['market_address'], "Incheon")
--- market/views/market_view/market_create_list_view.py
+++ market/views/market_view/market_create_list_view.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.permissions import IsReformer
+from django.core.exceptions import ObjectDoesNotExist
 from market.models import Market
 from market.serializers.market_serializers.market_serializer import MarketSerializer
 from users.models.reformer import Reformer
@@ -23,11 +24,11 @@ class MarketCreateListView(APIView):
         try:
             market = Market.objects.filter(reformer__user=request.user).select_related(
                 "reformer"
-            )
+            ).first()
             if not market:  # 마켓이 존재하지 않으면, 에러처리
-                raise Market.DoesNotExist
+                raise Market.DoesNotExist("리포머가 생성한 마켓이 존재하지 않습니다.")
 
-            serialized = MarketSerializer(instance=market, many=True)
+            serialized = MarketSerializer(instance=market)
             return Response(data=serialized.data, status=status.HTTP_200_OK)
         except Market.DoesNotExist:
             return Response(
@@ -43,13 +44,13 @@ class MarketCreateListView(APIView):
             # 리포머 프로필이 존재하는지 확인
             reformer = Reformer.objects.filter(user=request.user).first()
             if not reformer:
-                raise Reformer.DoesNotExist
+                raise Reformer.DoesNotExist("리포머 프로필이 존재하지 않습니다. 리포머 사용자만 호출 가능합니다.")
 
             # 리포머가 생성한 마켓이 몇개인지 확인
-            market_count = Market.objects.filter(reformer=reformer).count()
-            if market_count >= 5:  # 리포머 한명이 생성할 수 있는 마켓은 최대 5개
+            if Market.objects.filter(reformer=reformer).exists():
+                # 리포머 한명이 생성할 수 있는 마켓은 최대 1개
                 raise ValidationError(
-                    "This reformer exceeds the maximum number of markets"
+                    "리포머는 최대 1개의 마켓 까지만 생성이 가능합니다."
                 )
 
             serialized = MarketSerializer(
@@ -58,9 +59,9 @@ class MarketCreateListView(APIView):
             serialized.is_valid(raise_exception=True)
             serialized.save()
             return Response(data=serialized.data, status=status.HTTP_201_CREATED)
-        except Reformer.DoesNotExist:
+        except ObjectDoesNotExist as e:
             return Response(
-                data={"message": "reformer not found"}, status=status.HTTP_404_NOT_FOUND
+                data={"message": str(e)}, status=status.HTTP_404_NOT_FOUND
             )
         except ValidationError as e:
             return Response(

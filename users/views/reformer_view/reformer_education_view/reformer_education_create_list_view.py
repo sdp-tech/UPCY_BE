fbb008e308214b227ccbba385e058258832187bf
Scanf-s <calzone0404@gmail.com>
2025-01-13 18:58:21 +0900

    FIX Reformer edu, cert, ... APIs when request using GET Method

--- users/views/reformer_view/reformer_education_view/reformer_education_create_list_view.py
+++ users/views/reformer_view/reformer_education_view/reformer_education_create_list_view.py
@@ -1,72 +1,49 @@
-from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from core.exceptions import view_exception_handler
+from rest_framework.permissions import IsAuthenticated
 from users.models.reformer import Reformer, ReformerEducation
 from users.serializers.reformer_serializer.reformer_profile_serializer import (
     ReformerEducationSerializer,
 )
 
-
 class ReformerEducationCreateListView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @view_exception_handler
     def get(self, request):
-        try:
-            reformer_education = ReformerEducation.objects.filter(
-                reformer=request.user.reformer_profile
-            )
-            if not reformer_education:
-                raise Reformer.DoesNotExist
+        reformer = Reformer.objects.select_related("user").filter(user=request.user).first()
+        if not reformer:
+            raise ObjectDoesNotExist("Cannot found reformer with this user")
 
-            serializer = ReformerEducationSerializer(
-                instance=reformer_education, many=True
-            )
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-        except AttributeError as e:
-            return Response(
-                data={"message": str(e)}, status=status.HTTP_400_BAD_REQUEST
-            )
-        except Reformer.DoesNotExist:
-            return Response(
-                data={
-                    "message": "해당 User가 생성한 Reformer 프로필 정보가 존재하지 않습니다."
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
+        reformer_education = ReformerEducation.objects.filter(reformer=reformer)
+        if not reformer_education.exists():
+            raise ObjectDoesNotExist("No education data found for the reformer.")
 
+        serializer = ReformerEducationSerializer(
+            instance=reformer_education, many=True
+        )
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    @view_exception_handler
     def post(self, request):
-        try:
-            reformer = Reformer.objects.filter(user=request.user).first()
-            if not reformer:
-                raise Reformer.DoesNotExist
+        reformer = Reformer.objects.select_related("user").filter(user=request.user).first()
+        if not reformer:
+            raise ObjectDoesNotExist("Cannot found reformer with this user")
 
-            serializer = ReformerEducationSerializer(
-                data=request.data, context={"reformer": reformer}
-            )
-            if serializer.is_valid():
-                serializer.save()
-                return Response(
-                    data={"message": "successfully created"},
-                    status=status.HTTP_201_CREATED,
-                )
-            else:
-                raise ValueError(f"{serializer.errors}")
-        except (AttributeError, ValueError) as e:
+        serializer = ReformerEducationSerializer(
+            data=request.data, context={"reformer": reformer}
+        )
+        if serializer.is_valid():
+            serializer.save()
             return Response(
-                data={"message": str(e)}, status=status.HTTP_400_BAD_REQUEST
+                data={"message": "successfully created"},
+                status=status.HTTP_201_CREATED,
             )
-        except IntegrityError as e:
-            return Response(
-                data={"message": "데이터베이스 무결성 오류"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        except Reformer.DoesNotExist:
-            return Response(
-                data={
-                    "message": "해당 User가 생성한 Reformer 프로필 정보가 존재하지 않습니다."
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
+        else:
+            raise ValueError(f"{serializer.errors}")
--- users/views/reformer_view/reformer_profile_view.py
+++ users/views/reformer_view/reformer_profile_view.py
@@ -1,9 +1,11 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError, transaction
 from rest_framework import serializers, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.exceptions import view_exception_handler
 from users.models.reformer import Reformer
 from users.serializers.reformer_serializer.reformer_profile_serializer import (
     ReformerProfileSerializer,
@@ -18,119 +20,66 @@ class ReformerProfileView(APIView):
     permission_classes = [IsAuthenticated]
     user_service = UserService()
 
+    @view_exception_handler
     def get(self, request) -> Response:
         user = request.user  # 요청한 사용자 정보를 가져온다.
-        try:
-            reformer_profile = Reformer.objects.filter(
-                user=user
-            ).first()  # 사용자의 프로필에 연결되어 있는 리포머 프로필 데이터를 가져온다.
-            if not reformer_profile:  # 없다면 Exception
-                raise Reformer.DoesNotExist(
-                    "해당 사용자는 리포머 프로필이 등록되어 있지 않습니다."
-                )
-
-            serializer = ReformerProfileSerializer(
-                instance=reformer_profile, context={"request": request}
-            )
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Reformer.DoesNotExist as e:
-            return Response(
-                data={"message": str(e)},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        except Exception as e:
-            return Response(
-                data={"message": f"{str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        reformer_profile = Reformer.objects.filter(
+            user=user
+        ).first()  # 사용자의 프로필에 연결되어 있는 리포머 프로필 데이터를 가져온다.
+        if not reformer_profile:  # 없다면 Exception
+            raise Reformer.DoesNotExist(
+                "해당 사용자는 리포머 프로필이 등록되어 있지 않습니다."
             )
 
+        serializer = ReformerProfileSerializer(
+            instance=reformer_profile, context={"request": request}
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @view_exception_handler
     def post(self, request) -> Response:
         user = request.user
-        try:
-            serializer = ReformerProfileSerializer(
-                data=request.data, context={"request": request}
-            )
-            if serializer.is_valid():
-                serializer.save()
-                self.user_service.update_user_role(
-                    user=user, role="reformer"
-                )  # Reformer 프로필 등록 -> user role 변경
-                return Response(
-                    data={"message": "successfully created"},
-                    status=status.HTTP_201_CREATED,
-                )
-            else:
-                raise ValueError(f"{serializer.errors}")
-        except (AttributeError, ValueError) as e:
-            return Response(
-                data={"message": str(e)}, status=status.HTTP_400_BAD_REQUEST
-            )
-        except IntegrityError as e:
-            return Response(
-                data={"message": "데이터베이스 무결성 오류", "details": str(e)},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        except Exception as e:
-            return Response(
-                data={"message": "예기치 못한 오류 발생", "details": str(e)},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+        serializer = ReformerProfileSerializer(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid()
+        serializer.save()
+        self.user_service.update_user_role(
+            user=user, role="reformer"
+        )  # Reformer 프로필 등록 -> user role 변경
+        return Response(
+            data={"message": "successfully created"},
+            status=status.HTTP_201_CREATED,
+        )
 
+    @view_exception_handler
     def put(self, request) -> Response:
-        try:
-            user = request.user
-            reformer_profile = Reformer.objects.filter(user=user).first()
-            if not reformer_profile:
-                raise Reformer.DoesNotExist
+        user = request.user
+        reformer_profile = Reformer.objects.filter(user=user).first()
+        if not reformer_profile:
+            raise ObjectDoesNotExist("There is no reformer profile for this user.")
 
-            serializer = ReformerUpdateSerializer(
-                instance=reformer_profile, data=request.data, partial=True
-            )
-            if serializer.is_valid(raise_exception=True):
-                print(serializer.validated_data)
-                serializer.save()
-                return Response(
-                    data={"message": "successfully updated"}, status=status.HTTP_200_OK
-                )
-        except Reformer.DoesNotExist:
-            return Response(
-                data={
-                    "message": "Cannot find reformer profile that belongs to the user"
-                },
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        except (AttributeError, serializers.ValidationError) as e:
-            return Response(
-                data={"message": str(e)}, status=status.HTTP_400_BAD_REQUEST
-            )
-        except Exception as e:
-            return Response(
-                data={"message": f"{str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+        serializer = ReformerUpdateSerializer(
+            instance=reformer_profile, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            data={"message": "successfully updated"}, status=status.HTTP_200_OK
+        )
 
+    @view_exception_handler
     def delete(self, request):
         user = request.user
-        try:
-            reformer_profile = Reformer.objects.filter(user=user).first()
-            if not reformer_profile:
-                raise Reformer.DoesNotExist("리포머 프로필이 등록되어 있지 않습니다.")
+        reformer_profile = Reformer.objects.filter(user=user).first()
+        if not reformer_profile:
+            raise ObjectDoesNotExist("리포머 프로필이 등록되어 있지 않습니다.")
 
-            with transaction.atomic():
-                reformer_profile.delete()
-                self.user_service.update_user_role(
-                    user=user, role="user"
-                )  # reformer 프로필 삭제 -> user role 변경
-                return Response(
-                    data={"message": "successfully deleted"}, status=status.HTTP_200_OK
-                )
-        except Reformer.DoesNotExist as e:
+        with transaction.atomic():
+            reformer_profile.delete()
+            self.user_service.update_user_role(
+                user=user, role="user"
+            )  # reformer 프로필 삭제 -> user role 변경
             return Response(
-                data={"message": str(e)},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-        except Exception as e:
-            return Response(
-                data={"message": f"{str(e)}"},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                data={"message": "successfully deleted"}, status=status.HTTP_200_OK
             )

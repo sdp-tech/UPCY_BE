5af5690880ec02d1f589849d730dbe8b9a703c51
Scanf-s <calzone0404@gmail.com>
2025-01-14 13:32:38 +0900

    user signup validation

--- users/serializers/user_serializer/user_signup_serializer.py
+++ users/serializers/user_serializer/user_signup_serializer.py
@@ -1,29 +1,45 @@
 from rest_framework import serializers
-
+from django.core.validators import EmailValidator
+from django.contrib.auth.password_validation import validate_password
 from users.models.user import User
+
+
+def email_validation(email):
+    validator = EmailValidator()
+    try:
+        validator(email)
+    except serializers.ValidationError:
+        raise serializers.ValidationError("Invalid email address format.")
+
+    if User.objects.filter(email=email).exists():
+        raise serializers.ValidationError("Email already in use.")
+
+    return True
+
+def validate_password_field(password):
+    try:
+        validate_password(password)
+    except serializers.ValidationError:
+        raise serializers.ValidationError("Invalid password format")
+    return password
 
 
 class UserSignUpSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
-    def validate(self, attrs):
-        if User.objects.filter(email=attrs["email"]).exists():
-            raise serializers.ValidationError(
-                "User that using this email already exists."
-            )
+    def validate_email(self, value):
+        email_validation(value)
+        return value
 
-        if (
-            "nickname" in attrs
-            and User.objects.filter(nickname=attrs["nickname"]).exists()
-        ):
-            raise serializers.ValidationError(
-                "User that using this nickname already exists"
-            )
+    def validate_nickname(self, value):
+        if value == "":
+            raise serializers.ValidationError("Nickname cannot be empty.")
+        if User.objects.filter(nickname=value).exists():
+            raise serializers.ValidationError("Nickname already in use.")
+        return value
 
-        if attrs["password"] is None or attrs["password"] == "":
-            raise serializers.ValidationError("Password is required.")
-
-        return attrs
+    def validate_password(self, value):
+        return validate_password_field(value)
 
     class Meta:
         model = User
